000d0b972ec84dbd5d63d80b8fe4cc6c217b0ef9
Rai4a <student@students-mac.student.georgianc.on.ca>
2016-02-26 11:35:04 -0500

    Final Midterm

--- shoppingList/shoppingList/ViewController.swift
+++ shoppingList/shoppingList/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.maximumValue = 10
     }
 
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBAction func stepperValueChanged(sender: UIStepper) {
+        valueLabel.text = Int(sender.value).description
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
